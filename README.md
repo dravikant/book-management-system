652dbe3f64c2cda0acf011a7d1652b4a17fa77f4
Ravikant <rdindokar@vmware.com>
2022-07-08 15:43:27 +0530

    update mysql auth  and readme

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ MySQL Book Management System written in Golang
 This is an implementation of Book management system written in golang using mysql
 
 video link : https://youtu.be/jFfo23yIWac
+
+please note that code expects mysql server running with a database name "simplerest" already created on localhost:3306
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -12,7 +12,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "dravikant:R@vikant7@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "username:password@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
