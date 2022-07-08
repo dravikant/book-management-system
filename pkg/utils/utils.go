8e681fe1a8af6fa3649bd44b76938866b5f13af3
Ravikant <rdindokar@vmware.com>
2022-07-08 15:35:29 +0530

    added go files and postman collection

---
+++ pkg/utils/utils.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			fmt.Println("error while unmarshalling %s", err)
+			return
+		}
+	}
+}
